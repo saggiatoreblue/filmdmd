036d12d8fd97c346a1aced95d58e596dcdf92d0a
Michael Botelho <saggiatoreblue@gmail.com>
2015-05-23 16:44:34 -0400

    push to heroku

---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile : compile
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+
+if(env === 'development') {
+    mongoose.connect('mongodb://localhost/filmdmd');
+}
+else {
+    mongoose.connect('mongodb://saggiatoreblue:Antimatter12!@ds037252.mongolab.com:37252/filmdmd');
+}
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('db connection opened');
+});
+
+var messageSchema = mongoose.Schema({message:String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+    mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath', function(req,res){
+    res.render('partials/' + req.params.partialPath)
+
+});
+
+app.get('*', function(req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
+});
+
+
+var port = process.env.PORT || 3030;
+app.listen(port);
+
+console.log('Listening on port ' + port);
